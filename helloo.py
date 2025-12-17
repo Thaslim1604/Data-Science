eae66cbf1962061a7b9be93a9974d5a78ee87abb
Thaslim1604 <thaslimasulthana400@gmail.com>
2025-12-17 09:37:32 +0530

    task completed

---
+++ helloo.py
@@ -0,0 +1,34 @@
+#str1="listen"
+#str2="silent"
+#if sorted(str1)==sorted(str2):
+    #print("Anagram")
+#else:
+    #print("Not Anagram")   
+
+#import hi
+#print(hi.fun(10,40))    
+# s=[0,1,0,1,0]
+# zeros=[]
+# ones=[]
+# for i in s:
+    # if i==0:
+        # zeros.append(i)
+    # else:
+        # ones.append(i) 
+    # result=zeros+ones
+# print(result)       
+class person:
+    x=10
+    y=20
+    tup=(1,2,3)
+    c=[5,6,7]
+    d={100,200,300}
+    def func(self):
+        print("this is my class person")
+p1=person()
+p1.func()
+print(p1.x)
+print(p1.y)
+print(p1.c)
+print(p1.d)
+print(p1.tup)
---
+++ hi.py
@@ -0,0 +1,8 @@
+def fun(a,b):
+    i=100
+    if i==100:
+        print("i is",i)
+    return a+b
+
+def confidential():
+    print("This is confidential")    
